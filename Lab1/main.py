8c207657f33bce335d1047b9e3e355a891aabfd5
Ilya <hulahup@Lenovo-Legion-5-15ARH05-0a50cf29.localdomain>
2023-02-23 04:27:32 +0300

    Modified main and gitignore

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,1 +1,14 @@
+from calculate import calculate_two_numbers
+
+
 print("Hello World")
+
+try:
+    firstNumber = int(input("Enter the first number:"))
+    operation = input("Choose the operation (add, sub, div, mult):")
+    secondNumber  =int(input("Enter the second number:"))
+
+    print(f"Result: {calculate_two_numbers(firstNumber, secondNumber, operation)}")
+
+except Exception as exp:
+    print(exp.args)   
