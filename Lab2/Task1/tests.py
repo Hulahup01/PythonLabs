d72437cdef51b65a6d5d3d828fb2530d028a531e
Ilya <hulahup@Lenovo-Legion-5-15ARH05-0a50cf29.localdomain>
2023-03-22 23:36:41 +0300

    Added tests for func. get_amount_of_nondecl_sentences

--- Lab2/Task1/tests.py
+++ Lab2/Task1/tests.py
@@ -2,7 +2,9 @@ import unittest
 
 from tools.statistics import (
     get_amount_of_sentences,
+    get_amount_of_nondecl_sentences,
 )
+
 
 class TestAmountOfSentences(unittest.TestCase):
     def test_zero_result(self):
@@ -20,6 +22,24 @@ class TestAmountOfSentences(unittest.TestCase):
     def test_many_sentences_many_words(self):
         self.assertEqual(get_amount_of_sentences(
             "shibidibob, yes, yes! 2134 ! ; ... dob! dob! dob..."), 4)
-        
+
+
+class TestCountNonDeclarative(unittest.TestCase):
+    def test_zero_result(self):
+        self.assertEqual(get_amount_of_nondecl_sentences("  .. . . . .. !?? !?? ? !? ! ?! "), 0)
+        self.assertEqual(get_amount_of_nondecl_sentences(""), 0)
+
+    def test_one_non_declarative(self):
+        self.assertEqual(get_amount_of_nondecl_sentences("Word...!??!!??!?!"), 1)
+        self.assertEqual(get_amount_of_nondecl_sentences("A12..A12.!!!? B1500  ;;;"), 1)
+
+    def test_many_non_declarative_many_signs(self):
+        self.assertEqual(get_amount_of_nondecl_sentences("a...!?? ----!!?? a b!? c..!"), 3)
+
+    def test_many_sentences_many_words(self):
+        self.assertEqual(get_amount_of_nondecl_sentences("shibidibob, yes, yes! 2134 ! ; ... dob! dob! dob...",), 3)
+
+
+
 if __name__ == "__main__":
   unittest.main()
