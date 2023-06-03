26e448901ca2b13da6dd200ab6617f7e85e91727
Hulahup01 <ilya.deepak@yandex.ru>
2023-06-04 01:26:37 +0300

    make some view

--- Lab4/carshop/shop/views.py
+++ Lab4/carshop/shop/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Order, Product, ProductInstance, Manufacturer, Supplier
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+    num_products = Product.objects.all().count()
+    num_instances = ProductInstance.objects.all().count()
+    num_instances_available = ProductInstance.objects.filter(status__exact='a').count()
+    num_manuf = Manufacturer.objects.count()
+    return render(
+        request,
+        'index.html',
+        context={'num_products': num_products, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_manuf': num_manuf},
+    )
+
