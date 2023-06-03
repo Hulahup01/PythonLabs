f4aca4d059e808d40e8ae4331bf073e8482f2db5
Hulahup01 <ilya.deepak@yandex.ru>
2023-06-03 22:52:43 +0300

    Added urls view and templates

--- Lab4/carshop/shop/admin.py
+++ Lab4/carshop/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Product, ProductInstance, Manufacturer, Supplier
 
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(ProductInstance)
+admin.site.register(Manufacturer)
+admin.site.register(Supplier)
