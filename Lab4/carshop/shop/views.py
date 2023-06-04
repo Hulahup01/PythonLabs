f394085bb7932aa744a0ca1e9a3447372e80e992
Hulahup01 <ilya.deepak@yandex.ru>
2023-06-04 14:17:35 +0300

    Added authentication

--- Lab4/carshop/shop/views.py
+++ Lab4/carshop/shop/views.py
@@ -8,17 +8,21 @@ def index(request):
     num_instances = ProductInstance.objects.all().count()
     num_instances_available = ProductInstance.objects.filter(status__exact='a').count()
     num_manuf = Manufacturer.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
         request,
         'index.html',
         context={'num_products': num_products, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_manuf': num_manuf},
+                 'num_instances_available': num_instances_available, 'num_manuf': num_manuf, 'num_visits': num_visits},
     )
 
 
 class ProductListView(generic.ListView):
     model = Product
-    paginate_by = 1
+    paginate_by = 10
 
 
 class ProductDetailView(generic.DetailView):
